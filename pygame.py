9e34e14262d1b6d96fabff1787d8ed1ed0b7c63d
stoner07 <davidtiwari580@gmail.com>
2021-06-02 23:41:25 +0545

    pygame

--- pygame.py
+++ pygame.py
@@ -24,3 +24,38 @@ head.fillcolor("blue")
 head.penup()
 head.goto(0,0)
 head.direction="stop"
+
+#snake food
+food=turtle.Turtle()
+food.speed(0)
+food.shape("square")
+food.color("yellow")
+food.fillcolor("green")
+food.penup()
+food.ht()
+food.goto(0,200)
+food.st()
+
+#score board
+sb=turtle.Turtle()
+sb.shape("square")
+sb.fillcolor("black")
+sb.penup()
+sb.ht()
+sb.goto(-250,-250)
+sb.write("Score:0  |  Heighest Score:  0")
+
+def moveup() :
+    if head.direction!="down":
+        head.direction="up"
+def movedown() :
+    if head.direction!="up":
+        head.direction="down"
+def moveleft() :
+    if head.direction!="right":
+        head.direction="left"
+def moveright() :
+    if head.direction!="left":
+        head.direction="right"
+def movestop() :
+    head.direction="stop"
